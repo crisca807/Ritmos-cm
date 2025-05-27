1f88115d6f9b281d359ac8d8c6ffe04db804f5e7
cristanshi <128642694+crisca807@users.noreply.github.com>
2025-05-27 15:52:31 +0000

    se cambio banner de promocion

--- src/Pages/Home/Components/Banner.js
+++ src/Pages/Home/Components/Banner.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import '../styles/Banner.css'; // Archivo CSS donde se configurará la imagen
+import React from "react";
+import "../styles/Banner.css";
 
 const Banner = () => {
   return (
     <div className="unique-banner-container">
-      <div className="unique-logo"></div> {/* El fondo se maneja con CSS */}
+      <img
+        src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXFrMGtmYjMzbm81amc1a2dveDB2Zjk1dzdpaDVmemN6dGhnY2oyYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/f6baCLrbs30L3HL3Eq/giphy.gif"
+        alt="Man playing guitar"
+        className="banner-gif"
+      />
+      <div className="banner-text">
+        <h1>¡Descubre la guitarra perfecta para ti!</h1>
+        <p>
+          Instrumentos de alta calidad para todos los niveles. ¡Haz música hoy
+          mismo!
+        </p>
+      </div>
     </div>
   );
 };
