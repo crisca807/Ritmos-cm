214a6d30cc5e819ed998f6eac175e592cd687a9b
crisca807 <crisca807@gmail.com>
2025-07-24 21:01:58 -0500

    se corrigio

--- src/Pages/Home/Components/Navbar.js
+++ src/Pages/Home/Components/Navbar.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import '../styles/Navbar.css'; // Archivo CSS para estilos
-import { FaShoppingCart, FaSearch } from 'react-icons/fa'; // Importa los íconos necesarios
+import React, { useState } from 'react';
+import '../styles/Navbar.css';
+import { FaShoppingCart, FaSearch, FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         {/* El logo será agregado a través del CSS */}
       </div>
+
       <div className="navbar-search">
         <div className="search-container">
           <input type="text" placeholder="Buscar instrumentos..." className="search-input" />
@@ -16,14 +23,18 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-      <ul className="navbar-links">
-    
+
+      {/* Ícono hamburguesa para móvil */}
+      <div className="menu-toggle" onClick={handleToggleMenu}>
+        <FaBars />
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
         <li><a href="/percusion">Percusión</a></li>
         <li><a href="/cuerda">Cuerda</a></li>
         <li><a href="/viento">Viento</a></li>
         <li><a href="/accesorios">Accesorios</a></li>
-        <li><a href="/login" className="login-link">Iniciar </a></li>
-        {/* Ícono de carrillo */}
+        <li><a href="/login" className="login-link">Iniciar</a></li>
         <li className="cart-icon">
           <a href="/cart">
             <FaShoppingCart />
